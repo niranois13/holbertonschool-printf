7b9545e28732b994c70733f1571e0e0ee2b50f5c
niranois13 <8784@holbertonstudents.com>
2024-03-28 11:49:03 +0100

    adding print_percent

--- main.c
+++ main.c
@@ -8,40 +8,6 @@
  */
 int main(void)
 {
-	char *Z = "Zakaria";
-    char *str = "TEST un gros string de ouffff!!";
-    _printf("Let's print a simple sentence.\n");
-    printf("Let's print a simple sentence.\n");
-    _printf("%c\n", 'S');
-    printf("%c\n", 'S');
-    _printf("A char inside a sentence: %c. Did it work?\n", 'F');
-    printf("A char inside a sentence: %c. Did it work?\n", 'F');
-    _printf("Let's see if the cast is correctly done: %c. Did it work?\n", 48);
-    printf("Let's see if the cast is correctly done: %c. Did it work?\n", 48);
-    _printf("%s\n", "This sentence is retrieved from va_args!");
-    printf("%s\n", "This sentence is retrieved from va_args!");
-    _printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
-    printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
-    _printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
-    printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
-    _printf("%c%ccth %s%s a%cg%s: Y%sou %s no%ching Snow.%c", 'W', 'i', "some", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
-    printf("%c%ccth %s%s a%cg%s: Y%sou %s no%ching Snow.%c", 'W', 'i', "some", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
-    _printf("%%");
-    printf("%%");
-    _printf("Should print a single sign: %%.\n");
-    printf("Should print a single sign: %%.\n");
-    _printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
-    printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
-    _printf("%s%c%c%c%s%%%s%c\n", "Loading ", '.', '.', '.', " 99", "Please wait", '\n');
-    printf("%s%c%c%c%s%%%s%c\n", "Loading ", '.', '.', '.', " 99", "Please wait", '\n');
-    _printf("css%ccs%scscscs\n", 'T', "Test");
-    printf("css%ccs%scscscs\n", 'T', "Test");
-    _printf(str);
-    printf(str);
-    _printf("man gcc: \n%s\n", str);
-    printf("man gcc: \n%s\n", str);
-    _printf("NULL");
-    printf("NULL");
     _printf("%c", '\0');
     printf("%c", '\0');
     _printf("%");
